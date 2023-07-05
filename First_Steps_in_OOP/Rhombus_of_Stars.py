695ebe16c580d8833e47f6f39fe2c1db81b29025
ivo1337 <ivostamatov4@gmail.com>
2023-07-05 14:21:55 +0300

    exercises

---
+++ First_Steps_in_OOP/Rhombus_of_Stars.py
@@ -0,0 +1,8 @@
+def rhombus(n):
+    for i in range(1, n + 1):
+        print(" " * (n - i) + "* " * i)
+    for i in range(n - 1, 0, -1):
+        print(" " * (n - i) + "* " * i)
+
+number = int(input())
+rhombus(number)
