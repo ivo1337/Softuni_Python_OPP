672356d19c81b8cb421d41eea80662fe60d5fde7
ivo1337 <ivostamatov4@gmail.com>
2023-07-20 15:09:20 +0300

    exercises

---
+++  05_static_and_class_methods/shop.py
@@ -0,0 +1,10 @@
+class Shop:
+    def __init__(self, name, type, capacity):
+        self.name = name
+        self.type = type
+        self.capacity = capacity
+        self.items = {}
+
+    @classmethod
+    def small_shop(cls, name, type):
+        return name, type, 10
